dd330eaaa8592ea1fd1120024b70bd52e92a7ba0
Peter Schuck <peter@bendyworks.com>
2023-09-10 22:06:44 -0500

    Use Quil to display Game of Life board

--- src/game/life/core.clj
+++ src/game/life/core.clj
@@ -1,4 +1,6 @@
-(ns game.life.core)
+(ns game.life.core
+  (:require [quil.core :as qc]
+            [quil.middleware :as qm]))
 ; Boards are sets of x y coordinates
 (def board #{[0 0] [26 31] [8 12]})
 ; Takes a board of the game of life and returns the next time step
@@ -7,6 +9,24 @@
 
 (defn display-board [board]
   (println board))
+(defn setup []
+  ; initial state
+  (qc/frame-rate 1)
+  board)
+
+(defn draw [state]
+  (qc/background 0)
+  (qc/fill 255 0 0)
+  (doseq [[x y] state]
+    (qc/rect (* x 5) (* y 5) 5 5)))
+
+(qc/defsketch life
+  :title "Conway's Game of Life"
+  :size [200 200]
+  :setup setup
+  :middleware [qm/fun-mode]
+  :draw draw
+  :update step)
 
 (comment
   (def next-board (step board))

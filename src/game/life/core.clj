c87488001f0acdc27688e9b7213bb1324fcd6acc
Peter Schuck <peter@bendyworks.com>
2023-09-10 21:15:22 -0500

    Create base outline of game of life API and flow

--- src/game/life/core.clj
+++ src/game/life/core.clj
@@ -1,1 +1,14 @@
 (ns game.life.core)
+; Boards are sets of x y coordinates
+(def board #{[0 0] [26 31] [8 12]})
+; Takes a board of the game of life and returns the next time step
+(defn step [board]
+  board)
+
+(defn display-board [board]
+  (println board))
+
+(comment
+  (def next-board (step board))
+  (display-board board)
+  (display-board next-board))
